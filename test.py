842d2a7b00993529ee812e53f2992db6031c9e95
JustinHoyle <54443720+JustinHoyle@users.noreply.github.com>
2022-10-17 10:50:22 -0400

    counter

--- test.py
+++ test.py
@@ -66,9 +66,23 @@ def writeFile():
     text_file.write(saveLog)
     text_file.close()
 
+class threadCounter():
+    def __init__(self):
+        self._counter = [0,0,0,0,0]
+        # initialize lock
+        self._lock = Lock()
+    
+    def increment(self, i):
+        with self._lock:
+            self._counter[i] += 1
+    
+    def value(self, i):
+        with self._lock:
+            return self._counter[i]
+
 class processLifecycle(QObject):
 
-    def controlCycles(self):
+    def controlCycles(self, counter):
         global cycleCount
         global fileCommands
 
@@ -76,7 +90,7 @@ class processLifecycle(QObject):
 
         np.random.seed()
 
-        self.newCycle(thisProcess)
+        self.newCycle(thisProcess, counter)
         self.readyCycle(thisProcess)
         readFile()
 
@@ -87,15 +101,17 @@ class processLifecycle(QObject):
                 self.waitCycle(int(command[1]), int(command[2]), thisProcess)
         self.exitCycle(thisProcess)
 
-    def newCycle(self, process):
+    def newCycle(self, process, counter):
         global cycleCount
         global saveLog
         window.updateTextbox("Loading process " + process)
         saveLog+=("Loading process " + process + "\n")
+        counter.increment(0)
+        print(counter.value(0))
         start = time.perf_counter()
         for i in range(np.random.randint(5,15)):
             cycleCount+=1
-            window.cycleCountLabel.setText(QCoreApplication.translate("MainWindow", u"Cycle Count: " + str(cycleCount), None))
+            window.updateScreen(counter.value(0),counter.value(1),counter.value(2),counter.value(3),counter.value(4))
             QtTest.QTest.qWait(250)  
         stop = time.perf_counter()
         window.updateTextbox("Process " + process + f" loading finished in {stop - start:0.2f} seconds")
@@ -109,7 +125,7 @@ class processLifecycle(QObject):
         start = time.perf_counter()
         for i in range(np.random.randint(5,15)):
             cycleCount+=1
-            window.cycleCountLabel.setText(QCoreApplication.translate("MainWindow", u"Cycle Count: " + str(cycleCount), None))
+            window.updateScreen()
             QtTest.QTest.qWait(250)       
         stop = time.perf_counter()
         window.updateTextbox("Process " + process + f" in ready state {stop - start:0.2f} seconds")
@@ -123,7 +139,7 @@ class processLifecycle(QObject):
         start = time.perf_counter()
         for i in range(np.random.randint(min, max)):
             cycleCount+=1
-            window.cycleCountLabel.setText(QCoreApplication.translate("MainWindow", u"Cycle Count: " + str(cycleCount), None))
+            window.updateScreen()
             QtTest.QTest.qWait(250)
         stop = time.perf_counter()
         window.updateTextbox("Process " + process + f" ran in {stop - start:0.2f} seconds")
@@ -138,7 +154,7 @@ class processLifecycle(QObject):
         start = time.perf_counter()
         for i in range(np.random.randint(min, max)):
             cycleCount+=1
-            window.cycleCountLabel.setText(QCoreApplication.translate("MainWindow", u"Cycle Count: " + str(cycleCount), None))
+            window.updateScreen()
             QtTest.QTest.qWait(250)
         stop = time.perf_counter()
         window.updateTextbox(f"Process " + process + f" halted for {stop - start:0.2f} seconds")
@@ -153,7 +169,7 @@ class processLifecycle(QObject):
         start = time.perf_counter()
         for i in range(np.random.randint(5,15)):
             cycleCount+=1
-            window.cycleCountLabel.setText(QCoreApplication.translate("MainWindow", u"Cycle Count: " + str(cycleCount), None))
+            window.updateScreen()
             QtTest.QTest.qWait(250)
         stop = time.perf_counter()
         window.updateTextbox("Exit for process " + process + f" finished in {stop - start:0.2f} seconds")
@@ -199,26 +215,47 @@ class Main(QMainWindow, Ui_MainWindow):
     
     def runLongTask(self):
         # Step 2: Create a QThread object
-        self.thread1 = QThread()
-        self.thread2 = QThread()
+        counter = threadCounter()
+        self.threads = [QThread() for i in range(2)]
+        #self.thread1 = QThread()
+        #self.thread2 = QThread()
         # Step 3: Create a worker object
-        self.worker1 = processLifecycle()
-        self.worker2 = processLifecycle()
+        self.workers = [processLifecycle() for thread in self.threads]
+        #self.worker1 = processLifecycle()
+        #self.worker2 = processLifecycle()
         # Step 4: Move worker to the thread
-        self.worker1.moveToThread(self.thread1)
-        self.worker2.moveToThread(self.thread2)
+        for worker, thread in zip(self.workers, self.threads):
+            worker.moveToThread(thread)
+        #self.worker1.moveToThread(self.thread1)
+        #self.worker2.moveToThread(self.thread2)
         # Step 5: Connect signals and slots
-        self.thread1.started.connect(self.worker1.controlCycles)
-        self.thread2.started.connect(self.worker2.controlCycles)
+        for worker, thread in zip(self.workers, self.threads):
+            thread.started.connect(worker.controlCycles(counter))
+        #self.thread1.started.connect(self.worker1.controlCycles)
+        #self.thread2.started.connect(self.worker2.controlCycles)
         # Step 6: Start the thread
-        self.thread1.start()
-        self.thread2.start()
+        for thread in self.threads:
+            thread.start()
+        
+        #for thread in self.threads:
+            #thread.join()
+        #self.thread1.start()
+        #self.thread2.start()
     
-    def updateScreen(self, cycle, process, cpu, memory, disk):
-        self.cycleCountLabel.setText(QCoreApplication.translate("MainWindow", u"Cycle Count: " + str(cycle), None))
-        self.processCountLabel.setText(QCoreApplication.translate("MainWindow", u"Process Count: " + str(process), None))
-        self.CPUpercent.setText(QCoreApplication.translate("MainWindow", u"CPU: " + str(cpu) + '%', None))
-        self.memoryUse.setText(QCoreApplication.translate("MainWindow", u"Memory: " + str(memory) + ' MB', None))
+    
+    def updateScreen(self, new, ready, run, wait, exitV):
+        global cycleCount
+
+        self.newLabel.setText(QCoreApplication.translate("MainWindow", u"New: " + str(new), None))
+        self.readyLabel.setText(QCoreApplication.translate("MainWindow", u"Ready: " + str(ready), None))
+        self.runLabel.setText(QCoreApplication.translate("MainWindow", u"Run: " + str(run), None))
+        self.waitLabel.setText(QCoreApplication.translate("MainWindow", u"Wait: " + str(wait), None))
+        self.exitLabel.setText(QCoreApplication.translate("MainWindow", u"Exit: " + str(exitV), None))
+        self.cycleCountLabel.setText(QCoreApplication.translate("MainWindow", u"Cycle Count: " + str(cycleCount), None))
+        self.processCountLabel.setText(QCoreApplication.translate("MainWindow", u"Process Count: " + str(2), None))
+        # CPU % will likely always be zero due to how python is not closely related to the cpu
+        self.CPUpercent.setText(QCoreApplication.translate("MainWindow", f"CPU: {psutil.Process(os.getpid()).cpu_percent():0.2f}%", None))
+        self.memoryUse.setText(QCoreApplication.translate("MainWindow", f"Memory: {psutil.Process(os.getpid()).memory_info().rss / 1024 ** 2:0.2f} MB", None))
     
     def updateTextbox(self, text):
         self.proccessViewer.append(text)
